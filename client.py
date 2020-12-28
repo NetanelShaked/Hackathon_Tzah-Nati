92c356a445f78b93f7d27a6f3538322df3ba11bb
Netanel Shaked <shakolnak@gmail.com>
2020-12-28 14:43:21 +0200

    asd

--- client.py
+++ client.py
@@ -1,16 +1,38 @@
 import socket
+import struct
 
 if __name__ == '__main__':
     print("Client started, listening for offer requests...")
     UDP_IP = '255.255.255.255'
-    UPD_PORT = 5012
+    UPD_PORT = 13117
     cache_anointment = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    cache_anointment.bind((UDP_IP, UPD_PORT))
-    addr = None
-    while addr is not None:
+    cache_anointment.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    cache_anointment.bind(('', UPD_PORT))
+    stop = False
+    data = None
+    while not stop:
         data, addr = cache_anointment.recvfrom(1024)
+        if data is None:
+            continue
+        try:
+            msg_rec = struct.unpack('ibh', data)
+            if msg_rec[0] == -17973521 and msg_rec[1] == 2:
+                stop = True
+        except:
+            continue
     cache_anointment.close()
-
+    portnum = msg_rec[2]
+    print(int(portnum))
     tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    tcp_socket.connect(addr)
-
+    tcp_socket.connect((socket.gethostname(), portnum))
+    dontknowhowtocallthisvar = None
+    while dontknowhowtocallthisvar is None:
+        message = input("Enter:")
+        tcp_socket.sendall(bytes(message, "utf-8"))
+        try:
+            tcp_socket.settimeout(0.00001)
+            dontknowhowtocallthisvar = tcp_socket.recv(1024)
+        except:
+            pass
+    print(dontknowhowtocallthisvar)
+    tcp_socket.close()
--- server.py
+++ server.py
@@ -1,12 +1,52 @@
 import socket
+import struct
+import threading
+import time
 
 
 def send_broadcast_suggestion(socket_udp):
-    messege = bytes("offerAnnounment","utf-8")
-    socket_udp.sendto(messege, ("<broadcast>",5012))
+    message = struct.pack('Ibh', 0xfeedbeef, 0x2, 5112)
+    socket_udp.sendto(message, ("<broadcast>", 13117))
+
+
+def thread_send_enouncments(socket_udp):
+    threading.Timer(1.0, thread_send_enouncments, args=[socket_udp]).start()
+    send_broadcast_suggestion(upd_socket)
+
+
+def start_new_game(clientsocket):
+    print(clientsocket)
+    start_time = time.time()
+    char_counter = 0
+    while time.time() - start_time < 10:
+        clientsocket.settimeout(10)
+        try:
+            message = clientsocket.recv(1024)
+            char_counter += len(message)
+
+        except:
+            pass
+    print(char_counter)
+    answer=bytes("you hit"+str(char_counter)+" chars","utf-8")
+    clientsocket.sendall(answer)
+    clientsocket.close()
+
 
 if __name__ == '__main__':
+    SERVER_IP = socket.gethostname()
+    PORT_NUM = 5112
     print("Server started,listening on IP address 172.1.0.3")
     upd_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    upd_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+    sending_segestions_thread = threading.Thread(target=thread_send_enouncments, args=[upd_socket])
+    sending_segestions_thread.start()
+    # while True:
+    #     pass
+    tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcp_socket.bind((SERVER_IP, PORT_NUM))
+    tcp_socket.listen(2)
     while True:
-        send_broadcast_suggestion(upd_socket)
+        (clientsocket, address) = tcp_socket.accept()
+        game = threading.Thread(target=start_new_game, args=[clientsocket])
+        game.start()
