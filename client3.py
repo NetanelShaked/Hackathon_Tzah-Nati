f114a199271dec8247e9a364066f582ebcde33ec
Netanel Shaked <shakolnak@gmail.com>
2020-12-29 16:35:36 +0200

    asd

---
+++ client3.py
@@ -0,0 +1,45 @@
+import socket
+import struct
+import getch 
+
+
+if __name__ == '__main__':
+    print("Client started, listening for offer requests...")
+    UPD_PORT = 13117
+    cache_anointment = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    cache_anointment.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    cache_anointment.bind(('', UPD_PORT))
+    stop = False
+    data = None
+    while not stop:
+        data, addr = cache_anointment.recvfrom(1024)
+        if data is None:
+            continue
+        try:
+            msg_rec = struct.unpack('ibh', data)
+            if msg_rec[0] == -17973521 and msg_rec[1] == 2:
+                stop = True
+        except:
+            continue
+    cache_anointment.close()
+    print("Received offer from " + str(addr[0]) + " attempting to connect...")
+    portnum = msg_rec[2]
+    print("Port num " + str(int(portnum)))
+
+    tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcp_socket.connect((socket.gethostname(), 5112))
+
+    tcp_socket.sendall(bytes("THE CHANA'S", "utf-8"))
+
+    print(tcp_socket.recv(1024).decode("utf-8"))
+    server_message = None
+    while server_message is None:
+        message = getch.getche()
+        tcp_socket.sendall(bytes(message, "utf-8"))
+        try:
+            tcp_socket.settimeout(0.00001)
+            server_message = tcp_socket.recv(1024)
+        except:
+            pass
+    print(server_message.decode("utf-8"))
+    tcp_socket.close()
--- server.py
+++ server.py
@@ -4,9 +4,15 @@ import threading
 import time
 import concurrent.futures
 import random
+import multiprocessing
 
 
 def send_broadcast_suggestion(socket_udp):
+    """
+
+    :param socket_udp: udp_socket object
+    :return:
+    """
     message = struct.pack('Ibh', 0xfeedbeef, 0x2, 5112)
     socket_udp.sendto(message, ("<broadcast>", 13117))
 
@@ -16,21 +22,20 @@ def thread_send_enouncments(socket_udp):
     send_broadcast_suggestion(upd_socket)
 
 
-def start_new_game(clientsocket):
-    print(clientsocket)
+def start_new_game(clientside):
     start_time = time.time()
     char_counter = 0
     while time.time() - start_time < 10:
-        clientsocket.settimeout(10)
+        clientside.settimeout(10)
         try:
-            message = clientsocket.recv(1024)
+            message = clientside.recv(1024)
             char_counter += len(message)
 
         except:
             pass
     print(char_counter)
-    answer = bytes("************** you hit " + str(char_counter) + " chars **************", "utf-8")
-    clientsocket.sendall(answer)
+    # answer = bytes("************** you hit " + str(char_counter) + " chars **************", "utf-8")
+    # clientside.sendall(answer)
     return char_counter
 
 
@@ -42,7 +47,7 @@ if __name__ == '__main__':
     upd_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     upd_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
-    sending_segestions_thread = threading.Thread(target=thread_send_enouncments, args=[upd_socket])
+    sending_segestions_thread = multiprocessing.Process(target=thread_send_enouncments, args=(upd_socket,))
     sending_segestions_thread.start()
     # while True:
     #     pass
@@ -67,6 +72,7 @@ if __name__ == '__main__':
             print("Player 3 connected, waiting to Player 4 \ngroup name : " + group_name)
 
             (clientside, address_2) = tcp_socket.accept()
+            sending_segestions_thread.terminate()
             group_name = clientside.recv(1024).decode("utf-8")
             sockets_list.append((clientside, group_name))
             print("Last group is in, group name is " + group_name)
@@ -89,10 +95,10 @@ if __name__ == '__main__':
 
             for client, group_name in sockets_list:
                 client.sendall(start_message)
-            game1 = executor.submit(start_new_game, sockets_list[0])
-            game2 = executor.submit(start_new_game, sockets_list[1])
-            game3 = executor.submit(start_new_game, sockets_list[2])
-            game4 = executor.submit(start_new_game, sockets_list[3])
+            game1 = executor.submit(start_new_game, sockets_list[0][0])
+            game2 = executor.submit(start_new_game, sockets_list[1][0])
+            game3 = executor.submit(start_new_game, sockets_list[2][0])
+            game4 = executor.submit(start_new_game, sockets_list[3][0])
 
             game1_result = game1.result()
             game2_result = game2.result()
@@ -102,22 +108,31 @@ if __name__ == '__main__':
             buddies1_result = game1_result + game2_result
             buddies2_result = game3_result + game4_result
 
+            result_message = sockets_list[0][1] + " & " + sockets_list[1][1] + " get " + str(
+                buddies1_result) + " points\n" \
+                             + sockets_list[2][1] + " & " + sockets_list[3][1] + " get " + str(
+                buddies2_result) + " points\n"
+
             if buddies1_result == buddies2_result:
                 print("TEKO")
-                result_message = bytes("All Buddies get the same score, it a TEKO!", "utf-8")
+                result_message = bytes(result_message + "All Buddies get the same score, it a TEKO!", "utf-8")
 
             elif buddies1_result > buddies2_result:
                 print("Buddies 1 is winner")
-                result_message = bytes(
-                    sockets_list[0][1] + " " + sockets_list[1][1] + "win\n" + sockets_list[2][1] + " " +
-                    sockets_list[3][1] + " loss", "utf-8")
+                result_message = bytes(result_message +
+                                       sockets_list[0][1] + " " + sockets_list[1][1] + " win\n" + sockets_list[2][
+                                           1] + " " +
+                                       sockets_list[3][1] + " loss", "utf-8")
 
             else:
                 print("Buddies 2 is winner")
-                result_message = bytes(
-                    sockets_list[2][1] + " " +
-                    sockets_list[3][1] + "win\n" + sockets_list[0][1] + " " + sockets_list[1][1] + " loss", "utf-8")
+                result_message = bytes(result_message +
+                                       sockets_list[2][1] + " " +
+                                       sockets_list[3][1] + " win\n" + sockets_list[0][1] + " " + sockets_list[1][
+                                           1] + " loss", "utf-8")
 
             for client, group_name in sockets_list:
                 client.sendall(result_message)
                 client.close()
+            sending_segestions_thread = multiprocessing.Process(target=thread_send_enouncments, args=(upd_socket,))
+            sending_segestions_thread.start()
