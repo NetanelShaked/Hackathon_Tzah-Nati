a618191cc306419e7080b8b65fedbb203fe7cba3
Netanel Shaked <shakolnak@gmail.com>
2020-12-31 20:22:27 +0200

    Fixed some MAGIC s and general cosmetics

--- client.py
+++ client.py
@@ -4,12 +4,15 @@ import getch
 import time
 
 if __name__ == '__main__':
+    MAGIC_COOKIE = -17973521
+    MAGIC_KIND = 2
+
+    UDP_PORT = 13117
 
     while True:
         try:
             # Start looking for breadcast invitation
             print("Client started, listening for offer requests...")
-            UDP_PORT = 13117
             cache_anointment = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
             cache_anointment.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
             cache_anointment.bind(('', UDP_PORT))
@@ -22,7 +25,7 @@ if __name__ == '__main__':
                     continue
                 try:
                     msg_rec = struct.unpack('ibh', data)
-                    if msg_rec[0] == -17973521 and msg_rec[1] == 2:
+                    if msg_rec[0] == MAGIC_COOKIE and msg_rec[1] == MAGIC_KIND:
                         stop = True
                 except:
                     continue
@@ -38,15 +41,15 @@ if __name__ == '__main__':
                 # Sending our group name
                 tcp_socket.sendall(bytes("KeysCannon", "utf-8"))
                 tcp_socket.settimeout(10)
-                server_message=tcp_socket.recv(1024)
+                server_message = tcp_socket.recv(1024)
                 print(server_message.decode("utf-8"))
             except:
                 print("Connection failed, trying again.")
                 continue
             server_message = None
-            timer=time.time()
+            timer = time.time()
             # This loop will iterate until server send some message or 10.5 second is done
-            while server_message is None and time.time()-timer<10.5:
+            while server_message is None and time.time() - timer < 10.5:
                 message = getch.getche()
                 try:
                     tcp_socket.sendall(bytes(message, "utf-8"))
@@ -55,9 +58,9 @@ if __name__ == '__main__':
                 except socket.timeout:
                     pass
                 except ConnectionAbortedError:
-                    server_message=bytes("your computer was delay, tcp connection is gone", "utf-8")
+                    server_message = bytes("your computer was delay, tcp connection is gone", "utf-8")
                 except:
-                    server_message=bytes("unreason error","utf-8")
+                    server_message = bytes("unreason error", "utf-8")
             print(server_message.decode("utf-8"))
             tcp_socket.close()
         except:
